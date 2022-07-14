156ecda2eadb555294a3dd203341ca6ba0b6c98b
kroegh <Frederikkroegh1@hotmail.com>
2022-07-14 15:34:21 +0200

    .

---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoC.Models
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using WpfMVVM.Commands;
+
+namespace PoC.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public RelayCommand ClearSqlQueryCommand { get; private set; }
+        public RelayCommand ClearFilterCommand { get; private set; }
+        public RelayCommand ClearFilterByIdCommand { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string sqlQueryString = string.Empty;
+        private string filterString = string.Empty;
+        private string filterByIdString = string.Empty;
+        public string SqlQueryString
+        {
+            get
+            {
+                return sqlQueryString;
+            }
+            set
+            {
+                sqlQueryString = value;
+                OnPropertyChanged(nameof(SqlQueryString));
+            }
+        }
+
+        public string FilterString
+        {
+            get
+            {
+                return filterString;
+            }
+            set
+            {
+                filterString = value;
+                OnPropertyChanged(nameof(FilterString));
+            }
+        }
+
+        public string FilterByIdString
+        {
+            get
+            {
+                return filterByIdString;
+            }
+            set
+            {
+                filterByIdString = value;
+                OnPropertyChanged(nameof(FilterByIdString));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            ClearSqlQueryCommand = new RelayCommand(ClearSqlQuery, CanClearSqlQuery);
+            ClearFilterCommand = new RelayCommand(ClearFilter, CanClearFilter);
+            ClearFilterByIdCommand = new RelayCommand(ClearFilterById, CanClearFilterById);
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void ClearSqlQuery(object parameter)
+        {
+            SqlQueryString = string.Empty;
+        }
+
+        public bool CanClearSqlQuery(object parameter)
+        {
+            return true;
+        }
+
+        public void ClearFilter(object parameter)
+        {
+
+        }
+
+        public bool CanClearFilter(object parameter)
+        {
+            return true;
+        }
+
+        public void ClearFilterById(object parameter)
+        {
+
+        }
+
+        public bool CanClearFilterById(object parameter)
+        {
+            return true;
+        }
+    }
+}
